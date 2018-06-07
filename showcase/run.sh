91794ecb02f1591ffb97038d5495335dbf0d4279
Marek Hudík <marek.hudik@deutsche-boerse.com>
2018-06-07 13:21:27 +0200

    output filename

--- showcase/run.sh
+++ showcase/run.sh
@@ -5,4 +5,4 @@ BASE=`pwd`
 docker run -w /foo \
    -v $BASE:/foo \
    marekhudik/document-as-code \
-pandoc --self-contained --standalone --number-sections --table-of-contents main.md -o output.html --template github-markdown --include-before-body intro.html
+pandoc --self-contained --standalone --number-sections --table-of-contents main.md -o output_github-markdown.html --template github-markdown --include-before-body intro.html
